18c7d4f42c2ee465de4dfb12ba52af522e15ca9d
narutocyx <narutocyxlive@163.com>
2020-05-02 17:55:34 +0800

    no message

---
+++ 1bit_Games/Assets/Scripts/0_Flappy Ghost/FlappyGhost_CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappyGhost_CameraController : MonoBehaviour
+{
+    private Transform target;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if(target.position.x>transform.position.x)
+            transform.position = new Vector3(target.position.x, transform.position.y, -10);
+    }
+}
---
+++ 1bit_Games/Assets/Scripts/0_Flappy Ghost/FlappyGhost_PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappyGhost_PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    Rigidbody2D rb;    
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(0, jumpForce);
+        }
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+    }
+}
